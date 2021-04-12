2508827b7bc2efb0b8fc2b641dd25beea327a711
moisesmengo <moisesmengo@live.com>
2021-04-12 13:31:49 -0300

    s

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-// -> Service Worker 
+window.onload = () => {
+  'use strict';
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-  .then((reg) => console.log('service worker registered', reg))
-  .catch((err) => console.log('service worker not registered', err));
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('./sw.js');
+  }
 }
--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+var cacheName = 'hello-pwa';
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
+const filesToCache = [
   '/',
   '/css/style.css',
   '/css/',
@@ -13,50 +11,20 @@ const PRECACHE_URLS = [
   '/main.js'
 ];
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
 });
