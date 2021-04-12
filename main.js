cc994b39604047c351fba51986486c6fb607a883
moisesmengo <moisesmengo@live.com>
2021-04-12 13:43:02 -0300

    s

--- main.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then(function (registration) {
-      console.log('Service Work ok', registration.scope)
-    }).catch(function (err) {
-      console.log('Error service worker')
-    })
-}
